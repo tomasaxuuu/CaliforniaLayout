64cbee987238ef4f2b8f558c44556e3156799d5d
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-11-25 16:51:56 +0400

    final commit v2 (burger done)

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,29 @@
+
 const buttonSignUp = document.querySelector('.button-devices');
 let inputField =  document.querySelector('.input-devices');
 buttonSignUp.addEventListener("click", function () {
     alert(`Мы отправим письмо на вашу почту: ${inputField.value}`);
     inputField.value = '';
+});
+
+const burgerImg = document.querySelector(".burger-img");
+const nav = document.querySelector(".nav-menu");
+const burgerBtn = document.querySelector('.burger-menu');
+const liItem = document.querySelector('.nav-ul');
+
+burgerBtn.addEventListener("click", function() {
+    if(nav.classList.toggle('open')) {
+        burgerBtn.style = 'display: none';
+    } else {
+        burgerBtn.style = 'display: flex';
+    }
+    
+});
+
+liItem.addEventListener("click", function() {
+    if(nav.classList.toggle('open')) {
+        burgerBtn.style = 'display: none';
+    } else {
+        burgerBtn.style = 'display: flex';
+    }
 });
